93a79731c45044162da201a5b4b0c5fa8f0c68ef
yeyoung8805 <yyk8805@naver.com>
2022-08-25 15:45:20 +0900

    setting to omit filename extensions

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ const { VueLoaderPlugin } = require("vue-loader");
 
 //export
 module.exports = {
+  resolve: {
+    extensions: [".js", ".vue"],
+  },
+
   //parcel index.html 과 같이 parcel main.js 가 진입점 기준이 된다.
   //파일을 읽어들이기 시작하는 진입점 설정
   entry: "./src/main.js",
